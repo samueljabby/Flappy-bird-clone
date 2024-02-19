157ac4fcea352e708c7b443482a5b116f89f4ad1
samuel owen <owen84571@gmail.com>
2024-02-19 17:28:46 +0530

    added bird
    added bird using blit and transform

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ pygame.init()
 
 screen=pygame.display.set_mode((400,300))
 clock=pygame.time.Clock()
-#jus a tetst
+bird=pygame.image.load("Flappy-bird-clone/Bird.png")
+bird_rect=bird.get_rect(topleft=(0,0))
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+        
     
+    screen.blit(bird_rect,bird)
     pygame.display.update()
     clock.tick(60)
 
-    #this is another test for push
+    
 
     
